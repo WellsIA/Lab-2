e2b7d51468e9bcc621a3aab79d75208e28703264
WellsIA <46753165+WellsIA@users.noreply.github.com>
2019-01-23 10:16:15 -0500

    Add files via upload

---
+++ SquareTriangle.java
@@ -0,0 +1,23 @@
+// Square Triangle (IW & PM) 
+
+public class SquareTriangle
+{
+ public static void main(String[] rags)
+ {
+	System.out.println("#############################");
+	System.out.println("#             #             #");
+	System.out.println("#            # #            #");
+	System.out.println("#           #	#           #");
+	System.out.println("#          #     #          #");
+	System.out.println("#         #       #         #");
+	System.out.println("#        #         #        #");
+	System.out.println("#       #           #       #");
+	System.out.println("#      #             #      #");
+	System.out.println("#     #               #     #");
+	System.out.println("#    #                 #    #");
+	System.out.println("#   #                   #   #");
+	System.out.println("#  #                     #  #");
+	System.out.println("# #                       # #");
+	System.out.println("#############################");
+ }
+}
---
+++ Test5.java
@@ -0,0 +1,20 @@
+public class Test5
+{
+  public static void main(String[] args)
+  {
+    System.out.println("   ");
+    System.out.println("    ****");
+    System.out.println("  **  **");
+    System.out.println("********");
+    System.out.println("    ****          **");
+    System.out.println("    ******        **");
+    System.out.println("    ****************");
+    System.out.println("    ****************");
+    System.out.println("    **************");
+    System.out.println("      **********");
+    System.out.println("      **********");
+    System.out.println("        ******");
+    System.out.println();
+    System.out.println("This might be a duck.\n");
+    }
+}
